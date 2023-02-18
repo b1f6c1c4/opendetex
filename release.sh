34c7b96780443a1fbb9aa7222d58b53399444ef7
b1f6c1c4 <b1f6c1c4@gmail.com>
2023-02-17 23:22:45 -0500

    release script, debian

--- release.sh
+++ release.sh
@@ -1,38 +1,20 @@
 #!/bin/bash
 
-VERSION=$(head -1 README | perl -ne '/Version (\d+\.\d+\.\d+)/; print $1')
-NEXT_VERSION=$(echo $VERSION | perl -ne '/(\d+\.\d+\.)(\d+)/; print $1,$2+1')
+set -euxo pipefail
 
-echo "Last tag is $(git tag | tail -1)"
-echo "Releasing $VERSION, then bumping to $NEXT_VERSION"
+# rm -f opendetex-*.pkg.tar.zst opendetex.deb
+# 
+# makepkg
+# 
+# ln -s opendetex-*.pkg.tar.zst opendetex.pkg.tar.zst
 
-git tag | grep $VERSION
+rm -rf opendetex
+cp -r --preserve=mode pkg/opendetex .
+mkdir opendetex/DEBIAN
+cp control opendetex/DEBIAN/
+rm opendetex/.{BUILDINFO,MTREE,PKGINFO}
 
-if [[ $? == 0 ]]; then
-	echo "Error: this version already exists."
-	exit 2
-fi
-
-set -e
-
-if [[ ! -z $(git status --porcelain --untracked-files=no) ]]; then
-	echo "Error: make sure you committed everything"
-	exit 3
-fi
-
-CHANGELOG_DATE=$(LC_TIME=en_US date '+%B, %Y')
-sed -i "s/UNRELEASED/$CHANGELOG_DATE/" ChangeLog
-sed -i "s/-SNAPSHOT//" Makefile
-sed -i "s/ UNRELEASED//" README
-
-make clean test package
-git commit -a -S -m "Release $VERSION"
-git tag -s v$VERSION -m $VERSION HEAD
-
-echo -e "\n(Version $NEXT_VERSION) -- UNRELEASED" >> ChangeLog
-sed -i "s/$VERSION/$NEXT_VERSION-SNAPSHOT/" Makefile
-sed -i "s/$VERSION/$NEXT_VERSION UNRELEASED/" README
-git diff -U1 HEAD^
-git commit -a -m "Start developing $NEXT_VERSION"
-
-echo -e "\nInspect commits and run\ngit push origin master v$VERSION"
+docker run --pull always --rm -it \
+    -v "$PWD:/mnt" --workdir /mnt \
+    --user "$UID:$(id -g)" debian:stable \
+    dpkg-deb --root-owner-group --build opendetex
